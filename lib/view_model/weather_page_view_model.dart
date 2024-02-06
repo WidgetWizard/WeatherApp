4fdb139e2349d98529b77fcf2b270ca66a9cc2ed
Furkan Yıldırım <fy454525@gmail.com>
2024-02-06 14:19:21 +0300

    veri geliyor güzel şimdi düzgün birşekidle bildirimi göstermek var

--- lib/view_model/weather_page_view_model.dart
+++ lib/view_model/weather_page_view_model.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:weather_app/model/weather_five_days_with_three_hours_model.dart';
 import 'package:weather_app/service/background_image_service.dart';
 import '../model/weather_model.dart';
 import '../product/api/project_api.dart';
@@ -12,22 +13,25 @@ abstract class WeatherPageViewModel extends State<WeatherPageView>{
   final String _weatherApiKey = ProjectApi().getWeatherApi;
   final String _baseUrl = "https://api.openweathermap.org/data/2.5";
   final String randomImageUrl = RandomBackgroundImage().url;
-  Timer? _timer;
+  late Timer _timer;
   late final IWeatherService _weatherService;
   WeatherModel? weatherModel;
   late final NotificationService notificationService;
   late Future<void> initBackgroundImageAndWeatherFuture;
+  WeatherFiveDaysWithThreeHourModel? weatherThreeHoursModel;
+  late final IWeatherService _weatherThreeHoursService;
 
   @override
   void initState() {
     super.initState();
     initBackgroundImageAndWeatherFuture = _initBackgroundImageAndWeather();
+    _startTimer();
+    print("weatherThreeHoursModel :${weatherThreeHoursModel?.cityName}");
   }
 
   Future<void> _initBackgroundImageAndWeather() async {
     await initNotificationAndWeather().then((weather) {
       print(weather?.cityName);
-      startPeriodicNotifications();
     });
   }
 
@@ -38,35 +42,63 @@ abstract class WeatherPageViewModel extends State<WeatherPageView>{
     await _weatherService.getLocationWithPermission();
     await notificationService.initializeNotification(null);
     weather = await _weatherService.getWeatherData();
-    setState(() {
+    await initFiveDaysThreeHoursWeatherData().then((weather) {
+      print(weather?.temp);
+    });
+    setState(() async {
       weatherModel = weather;
     });
-    startPeriodicNotifications();
     return weather;
   }
 
-  void startPeriodicNotifications() { //todo:şaunda runla timersiz deniyorum
-    /*_timer = Timer.periodic(const Duration(seconds: 20), (timer) {
-      showNotification();
-    });*/
-    showNotification();
+  Future<WeatherFiveDaysWithThreeHourModel?> initFiveDaysThreeHoursWeatherData() async {
+    WeatherFiveDaysWithThreeHourModel model;
+    _weatherThreeHoursService = WeatherServiceForFiveDaysWithThreeHours(apiKey: _weatherApiKey, baseUrl: _baseUrl);
+    await _weatherThreeHoursService.getLocationWithPermission();
+    model = await _weatherThreeHoursService.getWeatherData();
+    print("model :${model.cityName}");
+    setState(() {
+      weatherThreeHoursModel = model;
+    });
+    return model;
   }
 
-  Future<void> showNotification() async {
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 20), (timer) {
+      _showNotification();
+    });
+  }
+
+  Future<void> _showNotification() async {
     await notificationService.showNotification(
-        title: "${weatherModel?.cityName}",
-        body: "${weatherModel?.mainCondition} ${weatherModel?.temp?.toInt()}°",
-        scheduled: true,
-        interval: 5,
+      title: "${weatherThreeHoursModel?.cityName}",
+      body: "${weatherModel?.mainCondition} ${weatherModel?.temp?.toInt()}°",
     );
   }
-/*  @override
+
+  @override
   void dispose() {
-    stopPeriodicNotifications();
+    _timer.cancel();
     super.dispose();
   }
-  void stopPeriodicNotifications() {
-    _timer?.cancel();
-  }*/
 }
 
+/*
+void startPeriodicNotifications() {
+  int k = 1;
+  _timer = Timer.periodic(const Duration(seconds: 20), (timer) {
+    if (weatherThreeHoursModel != null) {
+      if (k < (weatherThreeHoursModel?.mainCondition?.length ?? 0)) {
+        String? mainCondition = weatherThreeHoursModel?.mainCondition?[k];
+        double? temperature = weatherThreeHoursModel?.temp?[k].toInt().toDouble();
+        notificationService.showNotification(
+          title: "${weatherThreeHoursModel?.cityName}",
+          body: "$mainCondition ${temperature?.toInt()}°",
+        );
+        k += 2;
+      } else {
+        k = 1;
+      }
+    }
+  });
+}*/
